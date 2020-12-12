b7af0d6d212e7687122a429d99a0143123f1c05a
RHERNAM <69494624+RHERNAM@users.noreply.github.com>
2020-12-11 23:42:53 -0600

    Create Kardex.cs

---
+++ SistemaVentas/SistemaVentas/Clases/Entidates/Kardex.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using SCL;
+using SistemaVentas.Clases.SQL;
+
+namespace SistemaVentas.Clases.Entidates
+{
+    public class Kardex:SqlConexion
+    {
+
+        public static DataTable ConsultaKardexArticupoCodigo(string sCodigo)
+        {
+            return AccesoDatos.ToTable("Sp_Consulta_Kerdex_BuscarArticuloCodigo", CommandType.StoredProcedure,ConexionString,
+                ComandoSQL.Parameters.AddWithValue("@Codigo", sCodigo));
+        }
+    }
+}
