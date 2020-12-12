33d67c7609236241d16692cc4513978438610a6c
RHERNAM <69494624+RHERNAM@users.noreply.github.com>
2020-12-11 23:43:50 -0600

    Create UsuarioCache.cs

---
+++ SistemaVentas/SistemaVentas/Clases/Entidates/UsuarioCache.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVentas.Clases.Entidates
+{
+    public static class UsuarioCache
+    {
+
+        public static int Id_Usuario { get; set; }
+        public static int Nomina { get; set; }
+        public static string Nombre { get; set; }
+        public static string Apellido_Paterno { get; set; }
+        public static string Apellido_Materno { get; set; }
+        public static string CURP { get; set; }
+        public static string RFC { get; set; }
+        public static DateTime Fecha_Nacimiento { get; set; }
+        public static string Telefono { get; set; }
+        public static string Celular { get; set; }
+        public static string Email { get; set; }
+        public static string Usuario_Nombre { get; set; }
+        public static string Contraseña { get; set; }
+        public static int Puesto { get; set; }
+        public static string Calle { get; set; }
+        public static string Numero_Interno { get; set; }
+        public static string Numero_Exterior { get; set; }
+        public static string Entre_Calles { get; set; }
+        public static string Colonia { get; set; }
+        public static int Codigo_Postal { get; set; }
+        public static string Municipio { get; set; }
+        public static string Estado { get; set; }
+        public static string Pais { get; set; }
+        public static string PuestoUsuario { get; set; }
+        public static byte[] Imagen { get; set; }
+        public static int IdEmpresa { get; set; }
+        public static int IdSucursal { get; set; }
+        public static string Empresa { get; set; }
+        public static string Sucursal { get; set; }
+
+
+    }
+}
