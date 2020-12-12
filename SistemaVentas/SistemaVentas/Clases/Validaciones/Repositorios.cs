f0a88eef4e9599ff4186a7f208c4423e3d558301
RHERNAM <69494624+RHERNAM@users.noreply.github.com>
2020-12-11 23:45:21 -0600

    Create Repositorios.cs

---
+++ SistemaVentas/SistemaVentas/Clases/Validaciones/Repositorios.cs
@@ -0,0 +1,45 @@
+﻿using AccesDLL;
+using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVentas.Clases.Validaciones
+{
+    public class Repositorios
+    {
+        public static string[] PermisoUsuario { get; set; }
+
+        public static bool ValidarPermiso(string Permiso)
+        {
+            string[] Validacion = Permiso.Split(',');
+            foreach (var item in Validacion)
+            {
+                if (item!="" && PermisoUsuario.Contains(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        public static void ImprimirDirecto(ReportDocument reportDocument)
+        {
+            String PrintName = "";
+            if (PrintName==string.Empty)
+            {
+                PrintDocument printDocument = new PrintDocument();
+                PrintName = printDocument.PrinterSettings.PrinterName;
+            }
+
+            reportDocument.PrintOptions.PrinterName = PrintName;
+            reportDocument.PrintToPrinter(1, false, 0, 0);
+            Soporte.MsgInformacion("Se ha enviado el Ticket a Imprimir");
+
+        }
+    }
+}
