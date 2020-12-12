8bcd06f2751f031770f64155c94aacc162fd00a1
RHERNAM <69494624+RHERNAM@users.noreply.github.com>
2020-12-11 23:42:49 -0600

    Create BaseProp.cs

---
+++ SistemaVentas/SistemaVentas/Clases/Entidates/BaseProp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVentas.Clases.Entidates
+{
+    public static class BaseProp
+    {
+        
+        private static int _IdLog;      
+        private static string Modulo;
+        private static int _Nomina;
+        public  static int IdLog { get => _IdLog; set => _IdLog = value; }
+        public static string Modulo1 { get => Modulo; set => Modulo = value; }
+        public static int Nomina { get => _Nomina; set => _Nomina = value; }
+    }
+}
